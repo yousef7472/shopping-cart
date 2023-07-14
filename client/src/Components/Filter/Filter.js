f5a91a68ff78effd2abf8a114992e13e9e5ad86d
yousef7472 <yousef7shaban@gmail.com>
2023-07-14 19:58:13 +0300

    filter-comp

--- client/src/Components/Filter/Filter.js
+++ client/src/Components/Filter/Filter.js
@@ -1,5 +1,5 @@
 import "../../css/Filter/Filter.css"
-const Filter = () => {
+const Filter = (props) => {
     return ( 
         <aside className="filter-wrapper">
             <h2 className="filter-title">Filter</h2>
@@ -7,7 +7,7 @@ const Filter = () => {
                 <h3 className="num-of-products">number of products 4</h3>
                 <section className="filter-by-size">
                     <h3>Filter</h3>
-                    <select className="filter-select">
+                    <select className="filter-select" value={props.size} onChange={props.handleFilterBySize}>
                         <option value="All">All</option>
                         <option value="XS">XS</option>
                         <option value="S">S</option>
@@ -17,10 +17,10 @@ const Filter = () => {
                 </section>
                 <section>
                     <h3>Order</h3>
-                    <select className="filter-select">
+                    <select className="filter-select" value={props.order} onChange={props.handleFilterByOrder}>
                         <option value="latest">Latest</option>
-                        <option value="newest">Newest</option>
-                        <option value="heighest">Heighest</option>
+                        <option value="lowest">Lowest</option>
+                        <option value="highest">Heighest</option>
                     </select>
                 </section>
             </div>
